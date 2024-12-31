af275c48071fce8ce87a50726caf94dc4b9ea953
Demon_masterlqx <183842220@qq.com>
2024-12-31 15:44:43 +0800

    使用传统方法检测角点

--- src/detect_arrow/src/DetectArrow.cpp
+++ src/detect_arrow/src/DetectArrow.cpp
@@ -8,7 +8,12 @@
 using namespace std::chrono;
 using namespace std::placeholders;
 
+typedef std::vector<std::vector<cv::Point>> Counters;
+typedef std::vector<cv::Point> Counter;
+
 std::mutex mtxvideoget;
+
+const long double eps=1e-9;
 
 class Arrow_detector:public rclcpp::Node{
     public:
@@ -25,6 +30,9 @@ class Arrow_detector:public rclcpp::Node{
     // void MainArrowDetector(const sensor_msgs::msg::Image::SharedPtr msg);
     rclcpp::Node::SharedPtr node_shred_ptr;
     cv::Mat PreProgress(const cv::Mat & OriginalImage);
+    bool MainDetectArrow(const cv::Mat & OriginalImage);
+    bool TargetArrow(const cv::Mat & BinaryImage);
+    cv::Mat OriginalImage;
 };
 
 void Arrow_detector::CreatGetImageTimer(){
@@ -79,15 +87,117 @@ void Arrow_detector::GetImage(){
         return;
     }
     cv::Mat originalframe=cv_ptr->image;
+    originalframe.copyTo(OriginalImage);
     RCLCPP_INFO(this->get_logger(), "Get frame");
     // cv::imshow("Video",originalframe);
     // cv::waitKey(33);
-    PreProgress(originalframe);
+    MainDetectArrow(originalframe);
+
     mtxvideoget.unlock();
 }
 
-cv::Mat Arrow_detector::PreProgress(const cv::Mat & OriginalImage){
+bool Arrow_detector::TargetArrow(const cv::Mat & BinaryImage){
 
+    static int maxn=0,minn=1e9,approsiz=0;
+    static bool pre=0;
+    cv::Mat CounterImage;
+    Counters counters_;
+    Counter isarrow;
+
+    cv::Canny(BinaryImage,CounterImage,100.,200.,3,true);
+    
+    // cv::findContours(BinaryImage,counters_,cv::RETR_LIST,cv::CHAIN_APPROX_SIMPLE);
+
+    // cv::drawContours(OriginalImage,counters_,-1,cv::Scalar(225,0,0),1);
+
+    // cv::imshow("edge1",OriginalImage);
+    // cv::imshow("edge",edge);
+    // cv::waitKey(33);
+
+    
+
+    for(auto &counter_ :counters_){
+
+        cv::RotatedRect rotatedrect_=cv::minAreaRect(counter_);
+        Counter approxcurve;
+
+        double LengthWidthRatio= (std::min(rotatedrect_.size.width,rotatedrect_.size.height)<=eps ? 
+            -1 : std::max(rotatedrect_.size.width,rotatedrect_.size.height)/std::min(rotatedrect_.size.width,rotatedrect_.size.height));
+        int pixel_num=cv::contourArea(counter_);
+        cv::approxPolyDP(counter_,approxcurve,20,1);
+
+        RCLCPP_INFO(this->get_logger(),"LengthWidthRatio : %lf",LengthWidthRatio);
+        RCLCPP_INFO(this->get_logger(),"pixel_num : %d",pixel_num);
+        RCLCPP_INFO(this->get_logger(),"approxcurve size : %ld",approxcurve.size());
+
+        bool pixel_in=(pixel_num>=20000&&pixel_num<=80000);
+        bool lwratio=1.1<=LengthWidthRatio&&LengthWidthRatio<=(4/sqrt(2));
+        bool approxsize=(6<=approxcurve.size()&&approxcurve.size()<=10);
+
+        if(lwratio&&pixel_in){
+            maxn=std::max(maxn,pixel_num);
+            minn=std::min(minn,pixel_num);
+            
+            RCLCPP_INFO(this->get_logger(),"size of approxcurve: %ld",approxcurve.size());
+            RCLCPP_INFO(this->get_logger(),"max min pixel %d,%d",maxn,minn);
+            cv::drawContours(OriginalImage,Counters{approxcurve},-1,cv::Scalar(0,225,0),1);
+            cv::imshow("approxcurve",OriginalImage);
+            cv::waitKey(33);
+            pre=1;
+        }
+        else{
+            pre=0;
+            cv::drawContours(OriginalImage,Counters{approxcurve},-1,cv::Scalar(0,225,0),1);
+            cv::imshow("approxcurve",OriginalImage);
+            cv::waitKey(0);
+        }
+
+    }
+
+    return 1;
+}
+
+bool Arrow_detector::MainDetectArrow(const cv::Mat & OriginalImage){
+    cv::Mat Binary=PreProgress(OriginalImage);
+
+    bool HaveArrow=TargetArrow(Binary);
+    return 1;
+}
+
+cv::Mat Arrow_detector::PreProgress(const cv::Mat & OriginalImage){
+    std::vector<cv::Mat> SplitImage;
+    //通道顺序为BGR
+    cv::split(OriginalImage,SplitImage);
+    
+    cv::Mat GreyImage(SplitImage[0].size(),SplitImage[0].type()),BinaryImage,DilatedImage;
+    
+    cv::mixChannels(std::vector<cv::Mat>{SplitImage[0],SplitImage[2]},
+        std::vector<cv::Mat>{GreyImage},
+        std::vector<int>{
+            0,0,
+            1,0
+    });
+
+    double maxval,minval;
+
+    //控制二值化的参数
+    double threshholdk=0.5;
+    cv::minMaxLoc(GreyImage,&minval,&maxval);
+    minval=std::max(100.0,minval);
+    maxval=std::max(minval,maxval);
+
+    RCLCPP_INFO(this->get_logger(),"maxval of greyimage : %lf ,minval of greyimage : %lf ",maxval,minval);
+
+    cv::threshold(GreyImage,BinaryImage,maxval*threshholdk,300,cv::THRESH_BINARY);
+
+    cv::dilate(BinaryImage,DilatedImage,cv::getStructuringElement(cv::MORPH_ELLIPSE,cv::Size(3,3)));
+
+    cv::imshow("Pre",BinaryImage);
+    // cv::imshow("Ori",OriginalImage);
+    // cv::imshow("Gre",GreyImage);
+    cv::imshow("Dilated",DilatedImage);
+    cv::waitKey(33);
+    return DilatedImage;
 }
 
 int main (int argc,char* argv[]){
